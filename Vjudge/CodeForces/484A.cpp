6a1d12f7e9140b4b790c3ac196931c6f4039111e
Ahmed Hassan <96560599+ahmedMhassan10@users.noreply.github.com>
2023-10-11 00:08:33 +0300

    Add problem `Bits`

---
+++ Vjudge/CodeForces/484A.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(v) ((v).begin()), ((v).end())
+#define siz(v) ((int)((v).size()))
+#define clr(v, d) memset(v, d, sizeof(v))
+#define rep(i, v) for (int i = 0; i < sz(v); ++i)
+#define fo(i, n) for (int i = 0; i < (int)(n); ++i)
+#define fp(i, j, n) for (int i = (j); i <= (int)(n); ++i)
+#define fn(i, j, n) for (int i = (j); i >= (int)(n); --i)
+#define fvec(i, vec) for (auto i : vec)
+#define pb push_back
+#define MP make_pair
+#define mine(x, y, z) min(x, min(y, z))
+#define maxe(x, y, z) max(x, min(y, z))
+#define F first
+#define S second
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<double> vd;
+typedef vector<vi> vvi;
+typedef vector<vd> vvd;
+typedef vector<string> vs;
+typedef unsigned long long ull;
+typedef long long ll;
+const ll OO = 1e15, mod = 1e9 + 7, N = 1e5;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;cin>>t;
+    while(t--){
+    ll l,r;cin>>l>>r;
+    ll ans=l;
+    for(int i=0;i<61;i++){
+       if((1ll<<i)+ans<=r)ans|=(1ll<<i);
+    }
+    cout<<ans<<endl;
+    }
+}
