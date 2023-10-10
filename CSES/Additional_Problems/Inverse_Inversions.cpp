44a8c91cbbc5d4db83d147f54f4a96a824cceaec
Ahmed Hassan <96560599+ahmedMhassan10@users.noreply.github.com>
2023-10-10 23:48:19 +0300

    Add problem Inverse Inversions

---
+++ CSES/Additional_Problems/Inverse_Inversions.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+//#pragma GCC optimize("Ofast,no-stack-protector")
+//#pragma GCC optimize("unroll-loops")
+//#pragma GCC optimize("fast-math")
+
+#define clr(v, d)			memset(v, d, sizeof(v))
+#define ll long long
+#define int long long
+typedef tree< int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+#define setP(item) cout<<fixed<<setprecision(item);
+int ceilSqrt(int a){
+    return ::ceil(sqrtl(a));
+}
+int  divC(int a,int b){
+    return a/b+(a%b!=0);
+}
+int gcd(int a, int b) { return ((b == 0) ? a : gcd(b, a % b)); }
+int lcm(int a,int b){return (a*b)/gcd(a,b);}
+const ll OO =1e18, mod = 1e9+7,mod2=1e9+9,  N=1e6+10,M=25,MOD= 998244353, minOO=LLONG_MIN;
+const double EPS=1e-10;
+const double PI=acos(-1);
+const ll maximum=1000+10;
+const int NOT_VISITED = -1;
+
+///   هذا الحل من وحي خيال المؤلف واي تشابه بينه و بين اي حل اخر ما هو الا محض الصدفة
+
+void func(int index) {
+    ll n,m;cin>>n>>m;
+    ll val=n-1;
+    vector<int>vec;
+    ll cur=n;
+    ll begin=1;
+    for (int i = 1; i <= n ; ++i) {
+        if(val<=m){
+            vec.push_back(cur);
+            cur--;
+            m-=val;
+        }else{
+            vec.push_back(begin);
+            begin++;
+        }
+        val--;
+    }
+    for (auto item:vec) {
+        cout<<item<<' ';
+    }
+}
+signed main() {
+//    freopen("lcm.in","rt",stdin);
+    //freopen("output.txt","wt",stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t=1;
+//    cin>>t;
+    ll index=1;
+    while (t--){
+        func(index++);
+    }
+    return 0;
+}
