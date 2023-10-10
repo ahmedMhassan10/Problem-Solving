85dced1708390cf23d8fbfe3848f70df8a7a2123
Ahmed Hassan <96560599+ahmedMhassan10@users.noreply.github.com>
2023-10-10 23:46:22 +0300

    Add problem Distance Queries

---
+++ CSES/Tree_Algorithms/Distance_Queries.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define all(v) ((v).begin()), ((v).end())
+#define siz(v) ((int)((v).size()))
+#define clr(v, d) memset(v, d, sizeof(v))
+#define rep(i, v) for (int i = 0; i < siz(v); ++i)
+#define fo(i, n) for (int i = 0; i < (int)(n); ++i)
+#define fp(i, j, n) for (long long i = (j); i <= (long long)(n); ++i)
+#define fn(i, j, n) for (int i = (j); i >= (int)(n); --i)
+#define fvec(i, vec) for (auto i : vec)
+#define pb push_back
+#define MP make_pair
+#define mine(x, y, z) min(x, min(y, z))
+#define maxe(x, y, z) max(x, max(y, z))
+#define F first
+#define S second
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<ll,ll> pll;
+typedef pair<int,int> pi;
+
+typedef vector<pll> vp;
+typedef vector< long long> vll;
+typedef vector<double> vd;
+typedef vector<vi> vvi;
+typedef vector<vd> vvd;
+typedef vector<string> vs;
+typedef tree< pll, null_type, less<pll>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+#define setp(item) cout<<fixed<<setprecision(item);
+ll gcd(ll a, ll b) { return ((b == 0) ? a : gcd(b, a % b)); }
+ll lcm(ll a,ll b){return (a*b)/gcd(a,b);}
+const ll OO =1e18, mod = 1e9+7,mod2=1e9+9, N =2e5+5,M=30,MOD=998244353, minOO=LLONG_MIN;
+///   هذا الحل من وحي خيال المؤلف واي تشابه بينه و بين اي حل اخر ما هو الا محض الصدفة
+
+int n , ancestor[N][M], lvl[N];
+vector < int > g[N];
+void dfs( int node , int par)
+{
+    lvl[node] = lvl[par] + 1;
+    ancestor[node][0] = par;
+    for ( int j = 1 ; j < M ; j++)
+    {
+        int p = ancestor[node][j-1];
+        ancestor[node][j] = ancestor[p][j-1];
+    }
+    for(auto ch : g[node])
+    {
+        if(ch == par)
+            continue;
+        dfs(ch,node);
+    }
+}
+
+int findKthAncestor( int u , int k)
+{
+    for ( int j = M-1 ; j >= 0 ; j--)
+        if(k&(1<<j))
+            u = ancestor[u][j];
+    return u;
+}
+
+int getLCA( int u , int v)
+{
+    if(lvl[u] < lvl[v])
+        swap(u,v);
+    u = findKthAncestor(u,lvl[u]-lvl[v]);
+    if ( u == v)
+        return u;
+    for ( int i = M-1 ; i>= 0 ;i--)
+    {
+        if(ancestor[u][i] == ancestor[v][i])
+            continue;
+        u = ancestor[u][i];
+        v = ancestor[v][i];
+    }
+    return ancestor[u][0];
+}
+
+
+void func(int inde) {
+    ll n,q;cin>>n>>q;
+    fp(i,2,n){
+        ll u,v;cin>>u>>v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    dfs(1,0);
+    while (q--){
+        ll u,v;cin>>u>>v;
+        ll nu= getLCA(u,v);
+        cout<<abs(lvl[u]-lvl[nu])+ abs(lvl[v]-lvl[nu])<<'\n';
+    }
+
+}
+
+int main() {
+    //freopen("input.txt","rt",stdin);
+    //freopen("output.txt","wt",stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t=1;
+//    cin>>t;
+    ll index=1;
+    while (t--){
+        func(index++);
+    }
+    return 0;
+}
