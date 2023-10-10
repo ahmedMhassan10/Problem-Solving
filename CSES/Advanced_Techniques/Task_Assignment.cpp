484968ad632e150eafaaf26c869e7a3e3e8a3480
Ahmed Hassan <96560599+ahmedMhassan10@users.noreply.github.com>
2023-10-10 23:47:57 +0300

    Add problem Task Assignment

---
+++ CSES/Advanced_Techniques/Task_Assignment.cpp
@@ -0,0 +1,145 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+//#pragma GCC optimize("Ofast,no-stack-protector")
+//#pragma GCC optimize("unroll-loops")
+//#pragma GCC optimize("fast-math")
+
+#define clr(v, d)			memset(v, d, sizeof(v))
+#define ll long long
+#define int long long
+typedef tree< pair<int,int>, null_type, less<pair<int,int>>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+#define setP(item) cout<<fixed<<setprecision(item);
+int ceilSqrt(int a){
+    return ::ceil(sqrtl(a));
+}
+int  divC(int a,int b){
+    return a/b+(a%b!=0);
+}
+int gcd(int a, int b) { return ((b == 0) ? a : gcd(b, a % b)); }
+int lcm(int a,int b){return (a*b)/gcd(a,b);}
+const ll OO =1e18, mod = 1e9+7,mod2=1e9+9,  N=2e6+5,M=25,MOD= 998244353, minOO=LLONG_MIN;
+const double EPS=1e-10;
+const double PI=acos(-1);
+const ll maximum=1e6;
+const int NOT_VISITED = -1;
+
+///   هذا الحل من وحي خيال المؤلف واي تشابه بينه و بين اي حل اخر ما هو الا محض الصدفة
+const int INF = 1000 * 1000 * 1000;
+
+vector<int> assignment(vector<vector<int>>& a) {
+    int n = a.size();
+    int m = n * 2 + 2;
+    vector<vector<int>> f(m, vector<int>(m));
+    int s = m - 2, t = m - 1;
+    int cost = 0;
+    while (true) {
+        vector<int> dist(m, INF);
+        vector<int> p(m);
+        vector<bool> inq(m, false);
+        queue<int> q;
+        dist[s] = 0;
+        p[s] = -1;
+        q.push(s);
+        while (!q.empty()) {
+            int v = q.front();
+            q.pop();
+            inq[v] = false;
+            if (v == s) {
+                for (int i = 0; i < n; ++i) {
+                    if (f[s][i] == 0) {
+                        dist[i] = 0;
+                        p[i] = s;
+                        inq[i] = true;
+                        q.push(i);
+                    }
+                }
+            } else {
+                if (v < n) {
+                    for (int j = n; j < n + n; ++j) {
+                        if (f[v][j] < 1 && dist[j] > dist[v] + a[v][j - n]) {
+                            dist[j] = dist[v] + a[v][j - n];
+                            p[j] = v;
+                            if (!inq[j]) {
+                                q.push(j);
+                                inq[j] = true;
+                            }
+                        }
+                    }
+                } else {
+                    for (int j = 0; j < n; ++j) {
+                        if (f[v][j] < 0 && dist[j] > dist[v] - a[j][v - n]) {
+                            dist[j] = dist[v] - a[j][v - n];
+                            p[j] = v;
+                            if (!inq[j]) {
+                                q.push(j);
+                                inq[j] = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        int curcost = INF;
+        for (int i = n; i < n + n; ++i) {
+            if (f[i][t] == 0 && dist[i] < curcost) {
+                curcost = dist[i];
+                p[t] = i;
+            }
+        }
+        if (curcost == INF)
+            break;
+        cost += curcost;
+        for (int cur = t; cur != -1; cur = p[cur]) {
+            int prev = p[cur];
+            if (prev != -1)
+                f[cur][prev] = -(f[prev][cur] = 1);
+        }
+    }
+
+    vector<int> answer(n);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (f[i][j + n] == 1)
+                answer[i] = j;
+        }
+    }
+    return answer;
+}
+
+void func(int index) {
+    ll n;cin>>n;
+    vector<vector<int>>a(n,vector<int>(n));
+    for (int u = 0; u < n ; ++u) {
+        for (int v = 0; v < n ; ++v) {
+            cin>>a[u][v];
+        }
+    }
+    auto item= assignment(a);
+    ll res=0;
+    for (int i = 0; i < item.size(); ++i) {
+        res+=a[i][item[i]];
+    }
+    cout<<res<<endl;
+    for (int i = 0; i < item.size(); ++i) {
+        cout<<i+1<<" "<<item[i]+1<<endl;
+    }
+}
+signed main() {
+//    freopen("lcm.in","rt",stdin);
+    //freopen("output.txt","wt",stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t=1;
+//    cin>>t;
+    ll index=1;
+    while (t--){
+        func(index++);
+    }
+    return 0;
+}
