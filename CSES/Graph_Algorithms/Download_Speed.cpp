21613bb088cf71022a3653887a95dfbdd45fff03
Ahmed Hassan <96560599+ahmedMhassan10@users.noreply.github.com>
2023-10-10 23:45:34 +0300

    Add problem Download Speed

---
+++ CSES/Graph_Algorithms/Download_Speed.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+//#pragma GCC optimize("Ofast,no-stack-protector")
+//#pragma GCC optimize("unroll-loops")
+//#pragma GCC optimize("fast-math")
+
+#define clr(v, d)			memset(v, d, sizeof(v))
+#define ll long long
+#define int long long
+typedef tree< pair<int,int>, null_type, less<pair<int,int>>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+#define setP(item) cout<<fixed<<setprecision(item);
+int ceilSqrt(int a){
+    return ::ceil(sqrtl(a));
+}
+int  divC(int a,int b){
+    return a/b+(a%b!=0);
+}
+int gcd(int a, int b) { return ((b == 0) ? a : gcd(b, a % b)); }
+int lcm(int a,int b){return (a*b)/gcd(a,b);}
+const ll OO =1e18, mod = 1e9+7,mod2=1e9+9,  N=1e6+5,M=25,MOD= 998244353, minOO=LLONG_MIN;
+const double EPS=1e-12;
+const double PI=acos(-1);
+const ll maximum=1e5+3;
+const int NOT_VISITED = -1;
+
+///   هذا الحل من وحي خيال المؤلف واي تشابه بينه و بين اي حل اخر ما هو الا محض الصدفة
+
+struct FlowEdge {
+    int v, u;
+    ll cap, flow = 0;
+    FlowEdge(int v, int u, ll cap) : v(v), u(u), cap(cap) {}
+};
+
+struct Dinic {
+    const ll FLOW_INF = 1e18;
+    int n, m = 0, s, t;
+    vector<FlowEdge> edges;
+    vector<vector<int>> adj;
+    vector<int> level, ptr;
+    queue<int> q;
+    Dinic(int n, int s, int t) : n(n), s(s), t(t) {
+        adj.resize(n);
+        level.resize(n);
+        ptr.resize(n);
+    }
+    void addEdge(int u, int v, ll cap) {
+        edges.emplace_back(u, v, cap);
+        edges.emplace_back(v, u, 0);
+        adj[u].push_back(m++);
+        adj[v].push_back(m++);
+    }
+    bool bfs() {
+        while (!q.empty()) {
+            int v = q.front();
+            q.pop();
+            for (int id: adj[v]) {
+                if (edges[id].cap - edges[id].flow < 1) continue;
+                if (level[edges[id].u] != -1) continue;
+                level[edges[id].u] = level[v] + 1;
+                q.push(edges[id].u);
+            }
+        }
+        return level[t] != -1;
+    }
+    ll dfs(int v, ll pushed) {
+        if (pushed == 0) return 0;
+        if (v == t) return pushed;
+        for (int &cid = ptr[v]; cid < (int) adj[v].size(); cid++) {
+            int id = adj[v][cid], u = edges[id].u;
+            if (level[v] + 1 != level[u] || edges[id].cap - edges[id].flow < 1) continue;
+            ll tr = dfs(u, min(pushed, edges[id].cap - edges[id].flow));
+            if (tr == 0) continue;
+            edges[id].flow += tr;
+            edges[id ^ 1].flow -= tr;
+            return tr;
+        }
+        return 0;
+    }
+    ll flow() {
+        ll f = 0;
+        while (true) {
+            fill(level.begin(), level.end(), -1);
+            level[s] = 0;
+            q.push(s);
+            if (!bfs()) break;
+            fill(ptr.begin(), ptr.end(),0);
+            while (ll pushed = dfs(s, FLOW_INF))
+                f += pushed;
+        }
+        return f;
+    }
+};
+
+void func(int index) {
+    ll n,m;cin>>n>>m;
+    Dinic dinic= Dinic(n+1,1,n);
+    for (int i = 0; i < m; ++i) {
+        ll u,v,c;cin>>u>>v>>c;
+        dinic.addEdge(u,v,c);
+    }
+    cout<<dinic.flow();
+}
+signed main() {
+    //`freopen("zanzibar.in","rt",stdin);
+    //freopen("output.txt","wt",stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t=1;
+//    cin>>t;
+    ll index=1;
+    while (t--){
+        func(index++);
+    }
+    return 0;
+}
