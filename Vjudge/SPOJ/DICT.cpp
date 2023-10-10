84cd0c8837431e772c53eccb95b8992ab092fdfa
Ahmed Hassan <96560599+ahmedMhassan10@users.noreply.github.com>
2023-10-11 00:07:10 +0300

    Add problem `Search in the dictionary!`

---
+++ Vjudge/SPOJ/DICT.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(v) ((v).begin()), ((v).end())
+#define siz(v) ((int)((v).size()))
+#define clr(v, d) memset(v, d, sizeof(v))
+#define rep(i, v) for (int i = 0; i < sz(v); ++i)
+#define fo(i, n) for (int i = 0; i < (int)(n); ++i)
+#define fp(i, j, n) for (int i = (j); i <= (int)(n); ++i)
+#define fn(i, j, n) for (int i = (j); i >= (int)(n); --i)
+#define fvec(i, vec) for (auto i : vec)
+#define pb push_back
+#define MP make_pair
+#define mine(x, y, z) min(x, min(y, z))
+#define maxe(x, y, z) max(x, max(y, z))
+#define F first
+#define S second
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<pair<ll,ll>> vp;
+typedef vector<long long> vll;
+typedef vector<double> vd;
+typedef vector<vi> vvi;
+typedef vector<vd> vvd;
+typedef vector<string> vs;
+#define setp(item) cout<<fixed<<setprecision(item);
+ll gcd(ll a, ll b) { return ((b == 0) ? a : gcd(b, a % b)); }
+ll lcm(ll a,ll b){return (a*b)/gcd(a,b);}
+const ll OO = 1e9, mod = 1e9 + 7,mod2=1e9+9, N = 25005;
+struct Node{
+    Node *nxt[26];
+    int cnt;
+    vi vis;
+    Node()
+    {
+    	// any data You created up intialize it here
+        cnt = 0;
+        memset(nxt,0,sizeof nxt);
+    }
+}*root;
+void insert( string st,int index)
+{
+    Node *cur = root;
+    for( int i = 0 ; i < st.size();i++)
+    {
+        if(!cur->nxt[st[i]-'a'])
+            cur->nxt[st[i]-'a'] = new Node();
+        cur = cur->nxt[st[i]-'a'];
+        if(i!=st.size()-1)
+        cur->vis.pb(index);
+        cur->cnt++;
+    }
+}
+vs rec(N);
+
+void check( string st){
+    Node *cur = root;
+    for( int i = 0 ; i < st.size();i++)
+    {
+        if(!cur->nxt[st[i]-'a'])
+            {
+                cout<<"No match.\n";return ;
+            }
+                cur = cur->nxt[st[i]-'a'];
+    }
+    set<string>s;
+    fvec(item,cur->vis){
+        s.insert(rec[item]);
+    }
+    fvec(item,s)cout<<item<<'\n';
+}
+void testCase(){
+        root=new Node();
+        int num;cin>>num;
+        fo(i,num){
+            cin>>rec[i];
+            insert(rec[i],i);
+        }
+        int q;cin>>q;
+        int index=1;
+        while(q--){
+            cout<<"Case #"<<index<<":\n";
+            string str;
+            cin>>str;
+            check(str);
+            index++;
+        }
+}
+int main() {
+        // freopen("survive.in "," r ",stdin);
+        ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+        int t=1;
+        while(t--){
+            testCase();
+        }
+        return 0;
+}
