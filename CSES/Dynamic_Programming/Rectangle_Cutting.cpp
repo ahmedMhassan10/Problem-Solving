52083ee64c5deb698cc39383d888a38c2dfbd49b
Ahmed Hassan <96560599+ahmedMhassan10@users.noreply.github.com>
2023-10-10 23:44:39 +0300

    Add problem Rectangle Cutting

---
+++ CSES/Dynamic_Programming/Rectangle_Cutting.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define all(v) ((v).begin()), ((v).end())
+#define siz(v) ((int)((v).size()))
+#define clr(v, d) memset(v, d, sizeof(v))
+#define rep(i, v) for (int i = 0; i < siz(v); ++i)
+#define fo(i, n) for (int i = 0; i < (int)(n); ++i)
+#define fp(i, j, n) for (long long i = (j); i <= (long long)(n); ++i)
+#define fn(i, j, n) for (int i = (j); i >= (int)(n); --i)
+#define fvec(i, vec) for (auto i : vec)
+#define pb push_back
+#define MP make_pair
+#define mine(x, y, z) min(x, min(y, z))
+#define maxe(x, y, z) max(x, max(y, z))
+#define F first
+#define S second
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<ll,ll> pll;
+typedef pair<int,int> pi;
+
+typedef vector<pll> vp;
+typedef vector< long long> vll;
+typedef vector<double> vd;
+typedef vector<vi> vvi;
+typedef vector<vd> vvd;
+typedef vector<string> vs;
+typedef tree< pll, null_type, less<pll>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+#define setp(item) cout<<fixed<<setprecision(item);
+ll gcd(ll a, ll b) { return ((b == 0) ? a : gcd(b, a % b)); }
+ll lcm(ll a,ll b){return (a*b)/gcd(a,b);}
+const ll OO =1e18, mod = 1e9+7,mod2=1e9+9, N =2e5+5,M=30,MOD=998244353, minOO=LLONG_MIN;
+///   هذا الحل من وحي خيال المؤلف واي تشابه بينه و بين اي حل اخر ما هو الا محض الصدفة
+vector<vll>dp(501,vll(501,OO));
+ll func(int a,int b){
+    if(a > b)   swap(a, b);
+    if(dp[a][b] != OO)     return dp[a][b];
+    if(a == b)              return dp[a][b] = 0;
+    if(a == 1 || b == 1)    return dp[a][b] = (a == 1 ? b-1 : a-1);
+    for(int i = 1; i < a; i++)
+        dp[a][b] = min(dp[a][b], func(i, b) + func(a-i, b) + 1);
+    for(int i = 1; i < b; i++)
+        dp[a][b] = min(dp[a][b], func(a, i) + func(a, b-i) + 1);
+    return dp[a][b];
+}
+void solve(int inde) {
+    int a,b;cin>>a>>b;
+    cout<<func(a,b);
+}
+int main() {
+    //freopen("input.txt","rt",stdin);
+    //freopen("output.txt","wt",stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t=1;
+//    cin>>t;
+    ll index=1;
+    while (t--){
+        solve(index++);
+    }
+    return 0;
+}
