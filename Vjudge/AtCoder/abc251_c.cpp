285f311dda55391ddc3ce91eec0af473a2e13696
Ahmed Hassan <96560599+ahmedMhassan10@users.noreply.github.com>
2023-10-11 00:04:34 +0300

    Add problem `Poem Online Judge`

---
+++ Vjudge/AtCoder/abc251_c.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree< int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+#define clr(v, d)			memset(v, d, sizeof(v))
+#define ll long long
+
+#define int long long
+
+#define setp(item) cout<<fixed<<setprecision(item);
+#define PI 3.14159265358979323846264338327950
+
+int ceilSqrt(int a){
+    return ::ceil(sqrtl(a));
+}
+int  divC(int a,int b){
+    return a/b+(a%b!=0);
+}
+int gcd(int a, int b) { return ((b == 0) ? a : gcd(b, a % b)); }
+int lcm(int a,int b){return (a*b)/gcd(a,b);}
+const int OO =1e18, mod = 1e9+7,mod2=1e9+9, N =2e5+5,M=30,MOD= 998244353, minOO=LLONG_MIN;
+
+///   هذا الحل من وحي خيال المؤلف واي تشابه بينه و بين اي حل اخر ما هو الا محض الصدفة
+
+/// a33333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+
+void func(int index){
+    ll n;cin>>n;
+    set<string>se;
+    set<pair<int,int>>res;
+    vector<string>vec(n);
+    for (int i = 0; i < n; ++i) {
+        string str;cin>>str;
+        vec[i]=str;
+        ll c;cin>>c;
+        if(se.count(str))continue;
+        se.insert(str);
+        res.insert({c,-i});
+    }
+    auto p=*res.rbegin();
+    cout<<(-p.second)+1;
+}
+signed main() {
+    //freopen("input.txt","rt",stdin);
+    //freopen("output.txt","wt",stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t=1;
+//    cin>>t;
+    ll index=1;
+    while (t--){
+        func(index++);
+    }
+    return 0;
+}
