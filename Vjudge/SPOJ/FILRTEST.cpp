999ecbd5c548ddf18ec48aec8e058966b7da0e13
Ahmed Hassan <96560599+ahmedMhassan10@users.noreply.github.com>
2023-10-11 00:07:19 +0300

    Add problem `File Recover Testing`

---
+++ Vjudge/SPOJ/FILRTEST.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(v) ((v).begin()), ((v).end())
+#define siz(v) ((int)((v).size()))
+#define clr(v, d) memset(v, d, sizeof(v))
+#define rep(i, v) for (int i = 0; i < sz(v); ++i)
+#define fo(i, n) for (int i = 0; i < (int)(n); ++i)
+#define fp(i, j, n) for (int i = (j); i <= (int)(n); ++i)
+#define fn(i, j, n) for (int i = (j); i >= (int)(n); --i)
+#define fvec(i, vec) for (auto i : vec)
+#define pb push_back
+#define MP make_pair
+#define mine(x, y, z) min(x, min(y, z))
+#define maxe(x, y, z) max(x, max(y, z))
+#define F first
+#define S second
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<pair<ll,ll>> vp;
+typedef vector<long long> vll;
+typedef vector<double> vd;
+typedef vector<vi> vvi;
+typedef vector<vd> vvd;
+typedef vector<string> vs;
+#define setp(item) cout<<fixed<<setprecision(item);
+ll gcd(ll a, ll b) { return ((b == 0) ? a : gcd(b, a % b)); }
+ll lcm(ll a,ll b){return (a*b)/gcd(a,b);}
+const ll OO = 1e9, mod = 1e9 + 7,mod2=1e9+9, N = 1e6+1;
+void compute_prefix(string &s,vll &fail){
+    fp(i,1,siz(s)-1){
+        int j=fail[i-1];
+        while(j>0&& s[i]!=s[j])
+            j=fail[j-1];
+        if(s[i]==s[j])
+            fail[i]=j+1;
+        else fail[i]=0;
+    }
+}
+int main() {
+       // freopen("survive.in "," r ",stdin);
+        ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+        ll num;string str;
+        while(cin>>num,cin>>str){
+            if(num==-1&&str=="*")break;
+            vll fail(siz(str));
+            compute_prefix(str,fail);
+            ll nu=fail[siz(str)-1];
+            if(num<siz(str))cout<<"0\n";
+
+            else{
+             num-=siz(str);
+             cout<<num/(siz(str)-nu)+1<<'\n';
+            }
+        }
+        return 0;
+}
