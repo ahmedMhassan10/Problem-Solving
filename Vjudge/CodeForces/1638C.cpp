7c797a3be389cb9c083846c3a678985f8b48c5a3
Ahmed Hassan <96560599+ahmedMhassan10@users.noreply.github.com>
2023-10-11 00:08:06 +0300

    Add problem `Inversion Graph`

---
+++ Vjudge/CodeForces/1638C.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(v) ((v).begin()), ((v).end())
+#define siz(v) ((int)((v).size()))
+#define clr(v, d) memset(v, d, sizeof(v))
+#define rep(i, v) for (int i = 0; i < sz(v); ++i)
+#define fo(i, n) for (int i = 0; i < (int)(n); ++i)
+#define fp(i, j, n) for (int i = (j); i <= (int)(n); ++i)
+#define fn(i, j, n) for (int i = (j); i >= (int)(n); --i)
+#define fvec(i, vec) for (auto i : vec)
+#define pb push_back
+#define MP make_pair
+#define mine(x, y, z) min(x, min(y, z))
+#define maxe(x, y, z) max(x, max(y, z))
+#define F first
+#define S second
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<double> vd;
+typedef vector<vi> vvi;
+typedef vector<vd> vvd;
+typedef vector<string> vs;
+typedef unsigned long long ull;
+typedef long long ll;
+const ll OO = 1e5+3, mod = 1e8 , N = 1e5+5;
+int main() {
+   // freopen("survive.in "," r ",stdin);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;cin>>t;
+    while(t--){
+        int num;cin>>num;
+        vector<int>vec(num+1);
+        ll ans=0;
+        set <int>s;
+        fp(i,1,num){
+            int temp;cin>>temp;
+            s.insert(temp);
+            if(s.size()==*s.rbegin()&&*s.begin()==1)ans++;
+        }
+        cout<<ans<<'\n';
+    }
+}
