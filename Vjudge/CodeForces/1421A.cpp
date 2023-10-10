53ca9abb978c737d7d359d2594a1c2a5647fc388
Ahmed Hassan <96560599+ahmedMhassan10@users.noreply.github.com>
2023-10-11 00:09:17 +0300

    Add problem `XORwice`

---
+++ Vjudge/CodeForces/1421A.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)				((v).begin()), ((v).end())
+#define siz(v)				((int)((v).size()))
+#define clr(v, d)			memset(v, d, sizeof(v))
+#define rep(i, v)		for(int i=0;i<sz(v);++i)
+#define fo(i, n)		for(int i=0;i<(int)(n);++i)
+#define fp(i, j, n)	for(int i=(j);i<=(int)(n);++i)
+#define fn(i, j, n)	for(int i=(j);i>=(int)(n);--i)
+#define fvec(i,vec) for(auto i:vec)
+#define pb				push_back
+#define MP			make_pair
+#define mine(x,y,z) min(x,min(y,z))
+#define maxe(x,y,z) max(x,min(y,z))
+typedef long double   	  ld;
+typedef vector<int>       vi;
+typedef vector<double>    vd;
+typedef vector< vi >      vvi;
+typedef vector< vd >      vvd;
+typedef vector<string>    vs;
+typedef unsigned long long ull;
+typedef  long long ll;
+const ll OO =10e7;
+const int N=200005;
+int main()
+{
+        ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+        int t;cin>>t;
+        while(t--){
+            ll a,b;cin>>a>>b;
+            cout<<(a ^ b)<<'\n';
+        }
+}
+
