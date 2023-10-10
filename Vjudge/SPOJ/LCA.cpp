c7059b9fe6c55dc93024ff234400ad241e6d8845
Ahmed Hassan <96560599+ahmedMhassan10@users.noreply.github.com>
2023-10-11 00:06:44 +0300

    Add problem `Lowest Common Ancestor`

---
+++ Vjudge/SPOJ/LCA.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(v) ((v).begin()), ((v).end())
+#define siz(v) ((int)((v).size()))
+#define clr(v, d) memset(v, d, sizeof(v))
+#define rep(i, v) for (int i = 0; i < sz(v); ++i)
+#define fo(i, n) for (int i = 0; i < (int)(n); ++i)
+#define fp(i, j, n) for (int i = (j); i <= (int)(n); ++i)
+#define fn(i, j, n) for (int i = (j); i >= (int)(n); --i)
+#define fvec(i, vec) for (auto i : vec)
+#define pb push_back
+#define MP make_pair
+#define mine(x, y, z) min(x, min(y, z))
+#define maxe(x, y, z) max(x, max(y, z))
+#define F first
+#define S second
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<pair<ll,ll>> vp;
+typedef vector<long long> vll;
+typedef vector<double> vd;
+typedef vector<vi> vvi;
+typedef vector<vd> vvd;
+typedef vector<string> vs;
+#define setp(item) cout<<fixed<<setprecision(item);
+ll gcd(ll a, ll b) { return ((b == 0) ? a : gcd(b, a % b)); }
+ll lcm(ll a,ll b){return (a*b)/gcd(a,b);}
+const ll OO = 1e9, mod = 1e9 + 7,mod2=1e9+9, N = 5e5+10,M=25;
+
+
+int n , ancestor[N][M], lvl[N];
+vector < int > g[N];
+
+void dfs( int node , int par)
+{
+    lvl[node] = lvl[par] + 1;
+    ancestor[node][0] = par;
+    for ( int j = 1 ; j < M ; j++)
+    {
+        int p = ancestor[node][j-1];
+        ancestor[node][j] = ancestor[p][j-1];
+    }
+    for(auto ch : g[node])
+    {
+        if(ch == par)
+            continue;
+        dfs(ch,node);
+    }
+}
+
+int findKthAncestor( int u , int k)
+{
+    for ( int j = M-1 ; j >= 0 ; j--)
+        if(k&(1<<j))
+            u = ancestor[u][j];
+    return u;
+}
+
+int getLCA( int u , int v)
+{
+    if(lvl[u] < lvl[v])
+        swap(u,v);
+    u = findKthAncestor(u,lvl[u]-lvl[v]);
+    if ( u == v)
+        return u;
+    for ( int i = M-1 ; i>= 0 ;i--)
+    {
+        if(ancestor[u][i] == ancestor[v][i])
+            continue;
+        u = ancestor[u][i];
+        v = ancestor[v][i];
+    }
+    return ancestor[u][0];
+}
+void solve(int index){
+    cout<<"Case "<<index<<":\n";
+    cin>>n;
+    fp(i,1,n){
+        int k;cin>>k;
+        fo(j,k){
+            int u;cin>>u;
+            g[i].pb(u);
+            g[u].pb(i);
+        }
+    }
+    dfs(1,1);
+    int q;cin>>q;
+    while(q--){
+        int u,v;cin>>u>>v;
+        cout<<getLCA(u,v)<<"\n";
+    }
+}
+int main() {
+        // freopen("survive.in "," r ",stdin);
+        ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+        int t;cin>>t;
+        int index=1;
+        while(t--){
+            clr(g,0);clr(lvl,0);clr(ancestor,0);
+            solve(index);
+            ++index;
+        }
+        return 0;
+}
