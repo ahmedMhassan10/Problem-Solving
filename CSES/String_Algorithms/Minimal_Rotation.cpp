d61760618dc6613a1ecd80c01481c0330807e184
Ahmed Hassan <96560599+ahmedMhassan10@users.noreply.github.com>
2023-10-10 23:47:17 +0300

    Add problem Minimal Rotation

---
+++ CSES/String_Algorithms/Minimal_Rotation.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+//#pragma GCC optimize("Ofast,no-stack-protector")
+//#pragma GCC optimize("unroll-loops")
+//#pragma GCC optimize("fast-math")
+
+#define clr(v, d)			memset(v, d, sizeof(v))
+#define ll long long
+#define int long long
+typedef tree< pair<int,int>, null_type, less<pair<int,int>>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+#define setP(item) cout<<fixed<<setprecision(item);
+int ceilSqrt(int a){
+    return ::ceil(sqrtl(a));
+}
+int  divC(int a,int b){
+    return a/b+(a%b!=0);
+}
+int gcd(int a, int b) { return ((b == 0) ? a : gcd(b, a % b)); }
+int lcm(int a,int b){return (a*b)/gcd(a,b);}
+const ll OO =1e18+10, mod = 1e9+7,mod2=1e9+9,  N=2e5+5,M=25,MOD= 998244353, minOO=LLONG_MIN;
+const double EPS=1e-10;
+const double PI=acos(-1);
+const ll maximum=2500+6;
+const int NOT_VISITED = -1;
+int duval(string & s) {
+    int n = s.size();
+    int i = 0;
+    ll res=0;
+    vector<string> factorization;
+    while (i < n/2) {
+        res=i;
+        int j = i + 1, k = i;
+        while (j < n && s[k] <= s[j]) {
+            if (s[k] < s[j])
+                k = i;
+            else
+                k++;
+            j++;
+        }
+        while (i <= k) {
+            i += j - k;
+        }
+    }
+    return res;
+}
+void func(int index){
+    string str;cin>>str;
+    ll n=str.size();
+    str+=str;
+    auto idx=duval(str);
+    for (int i = idx; i < idx+n; ++i) {
+        cout<<str[i];
+    }
+}
+
+signed main() {
+//    freopen("ysys.in","rt",stdin);
+    //freopen("output.txt","wt",stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t=1;
+//    cin>>t;
+    ll index=1;
+    while (t--){
+        func(index++);
+    }
+    return 0;
+}
