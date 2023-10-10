322c215f4822909c40afbaf73cd785de8a6ea126
Ahmed Hassan <96560599+ahmedMhassan10@users.noreply.github.com>
2023-10-10 23:43:35 +0300

    Add problem Apartments

---
+++ CSES/Sorting_and_Searching/Apartments.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree< int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+#define int long long
+#define ll long long
+
+#define setp(item) cout<<fixed<<setprecision(item);
+#define PI 3.14159265358979323846264338327950
+int ceilSqrt(int a){
+    return ::ceil(sqrtl(a));
+}
+int  divC(int a,int b){
+    return a/b+(a%b!=0);
+}
+int gcd(int a, int b) { return ((b == 0) ? a : gcd(b, a % b)); }
+int lcm(int a,int b){return (a*b)/gcd(a,b);}
+const int OO =1e15, mod = 1e9+7,mod2=1e9+9, N =2e5+5,M=30,MOD= 998244353, minOO=LLONG_MIN;
+///   هذا الحل من وحي خيال المؤلف واي تشابه بينه و بين اي حل اخر ما هو الا محض الصدفة
+/// :-)
+/// a33333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+void func(int index) {
+    ll n,m,k;cin>>n>>m>>k;
+    multiset<int>app,dep;
+    for (int i = 0; i < n; ++i) {
+        ll temp;cin>>temp;
+        app.insert(temp);
+    }
+    for (int i = 0; i < m; ++i) {
+        ll temp;cin>>temp;
+        dep.insert(temp);
+    }
+    ll res=0;
+    for (auto item:app) {
+        auto it=dep.lower_bound(item-k);
+        if(it==dep.end())continue;
+        if(*it<=item+k){
+            res++;
+            dep.erase(it);
+        }
+    }
+    cout<<res;
+}
+signed main() {
+    //freopen("input.txt","rt",stdin);
+    //freopen("output.txt","wt",stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t=1;
+//    cin>>t;
+    int index=1;
+    while (t--){
+        func(index++);
+    }
+    return 0;
+}
