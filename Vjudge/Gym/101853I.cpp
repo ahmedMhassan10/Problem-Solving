eac4f6539e465bd60ebe6af4280b54ea2a871140
Ahmed Hassan <96560599+ahmedMhassan10@users.noreply.github.com>
2023-10-11 00:09:59 +0300

    Add problem `Circles`

---
+++ Vjudge/Gym/101853I.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+typedef unsigned long long ull;
+typedef  long long ll;
+ 
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int t;cin>>t;
+    while(t--){
+        int a,b,d;cin>>a>>b>>d;
+        double A2=d*(double)d/2;
+        printf("%.6lf\n",A2);
+    }
+}
