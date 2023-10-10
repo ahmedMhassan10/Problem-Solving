8223f37b8058b7228e6b7ae1f239913a4a131f78
Ahmed Hassan <96560599+ahmedMhassan10@users.noreply.github.com>
2023-10-11 00:06:36 +0300

    Add problem `STONE GAME`

---
+++ Vjudge/CodeChef/RESN04.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(v) ((v).begin()), ((v).end())
+#define siz(v) ((int)((v).size()))
+#define clr(v, d) memset(v, d, sizeof(v))
+#define rep(i, v) for (int i = 0; i < sz(v); ++i)
+#define fo(i, n) for (int i = 0; i < (int)(n); ++i)
+#define fp(i, j, n) for (long long i = (j); i <= (long long)(n); ++i)
+#define fn(i, j, n) for (int i = (j); i >= (int)(n); --i)
+#define fvec(i, vec) for (auto i : vec)
+#define pb push_back
+#define MP make_pair
+#define mine(x, y, z) min(x, min(y, z))
+#define maxe(x, y, z) max(x, max(y, z))
+#define F first
+#define S second
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<pair<ll,ll>> vp;
+typedef vector<long long> vll;
+typedef vector<double> vd;
+typedef vector<vi> vvi;
+typedef vector<vd> vvd;
+typedef vector<string> vs;
+#define setp(item) cout<<fixed<<setprecision(item);
+ll gcd(ll a, ll b) { return ((b == 0) ? a : gcd(b, a % b)); }
+ll lcm(ll a,ll b){return (a*b)/gcd(a,b);}
+const ll OO = 1e9, mod = 1e9 + 7,mod2=1e9+9, N = 5e5+10,M=25;
+
+int main() {
+        // freopen("survive.in "," r ",stdin);
+        ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+        int t;cin>>t;
+        while(t--){
+            ll num;cin>>num;
+            vll vec(num);
+            ll sum=0;
+            fo(i,num){
+                cin>>vec[i];
+                vec[i]/=(i+1);
+                sum+=vec[i];
+            }
+            if(sum%2==1){
+                cout<<"ALICE\n";
+            }else{
+                cout<<"BOB\n";
+            }
+        }
+}
