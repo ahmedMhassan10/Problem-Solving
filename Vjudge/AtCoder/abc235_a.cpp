d0c54a5418292dd634935f5d8c881b2dce599765
Ahmed Hassan <96560599+ahmedMhassan10@users.noreply.github.com>
2023-10-11 00:04:40 +0300

    Add problem `Rotate`

---
+++ Vjudge/AtCoder/abc235_a.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree< int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+#define clr(v, d)			memset(v, d, sizeof(v))
+#define ll long long
+
+#define int long long
+
+#define setp(item) cout<<fixed<<setprecision(item);
+#define PI 3.14159265358979323846264338327950
+
+int ceilSqrt(int a){
+    return ::ceil(sqrtl(a));
+}
+int  divC(int a,int b){
+    return a/b+(a%b!=0);
+}
+int gcd(int a, int b) { return ((b == 0) ? a : gcd(b, a % b)); }
+int lcm(int a,int b){return (a*b)/gcd(a,b);}
+const int OO =1e18, mod = 1e9+7,mod2=1e9+9, N =2e5+5,M=30,MOD= 998244353, minOO=LLONG_MIN;
+
+///   هذا الحل من وحي خيال المؤلف واي تشابه بينه و بين اي حل اخر ما هو الا محض الصدفة
+
+/// a33333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+
+void func(int index){
+    vector<int>vec(3);
+    ll num;cin>>num;
+    ll ind=0;
+    while (num){
+        vec[ind]=num%10;
+        num/=10;
+        ind++;
+    }
+    ll res=0;
+    for (int i = 0; i < 3; ++i) {
+        ll cur=0;
+        ll total=100;
+        for (int j = 0; j < 3; ++j) {
+            cur+=total*vec[(i+j)%3];
+            total/=10;
+        }
+        res+=cur;
+    }
+    cout<<res;
+}
+signed main() {
+    //freopen("input.txt","rt",stdin);
+    //freopen("output.txt","wt",stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t=1;
+//    cin>>t;
+    ll index=1;
+    while (t--){
+        func(index++);
+    }
+    return 0;
+}
