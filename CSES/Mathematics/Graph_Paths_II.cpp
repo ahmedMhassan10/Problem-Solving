b864d43e558a0409e83cfa367101acd09e020c6b
Ahmed Hassan <96560599+ahmedMhassan10@users.noreply.github.com>
2023-10-10 23:46:59 +0300

    Add problem Graph Paths II

---
+++ CSES/Mathematics/Graph_Paths_II.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define clr(v, d)			memset(v, d, sizeof(v))
+#define ll long long
+#define int long long
+typedef tree< pair<ll,ll>, null_type, less<pair<ll,ll>>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+#define setp(item) cout<<fixed<<setprecision(item);
+#define PI 3.14159265358979323846264338327950
+
+int ceilSqrt(int a){
+    return ::ceil(sqrtl(a));
+}
+int  divC(int a,int b){
+    return a/b+(a%b!=0);
+}
+int gcd(int a, int b) { return ((b == 0) ? a : gcd(b, a % b)); }
+int lcm(int a,int b){return (a*b)/gcd(a,b);}
+const ll OO =2e18, mod = 1e9+7,mod2=1e9+9, N =5000+5,M=30,MOD= 998244353, minOO=LLONG_MIN;
+
+///   هذا الحل من وحي خيال المؤلف واي تشابه بينه و بين اي حل اخر ما هو الا محض الصدفة
+
+/// a33333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+vector<vector<int>> mul(vector<vector<int>> &a, vector<vector<int>> &b) {
+    int n = a.size(), m = b[0].size(), f = b.size();
+    vector<vector<int>> c(n, vector<int>(m, OO));
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            for (int k = 0; k < f; k++)
+            {
+
+                c[i][j] =min(c[i][j], ((a[i][k]+b[k][j])));
+            }
+    return c;
+}
+vector<vector<int>> idendity(int n) {
+    vector<vector<int>> arr(n, vector<int>(n, 0));
+    for (int i = 0; i < n; i++) arr[i][i] = 1;
+    return arr;
+}
+vector<vector<int>> expo(vector<vector<int>> a, int x) {
+    int n = a.size();
+    if (x <= 0) return idendity(n);
+    if (x == 1) return a;
+    vector<vector<int>> newMat = expo(a, x / 2);
+    vector<vector<int>> ans = mul(newMat, newMat);
+    if (x % 2 == 1) ans = mul(ans, a);
+    return ans;
+}
+void func(int index){
+    ll n,m,k;cin>>n>>m>>k;
+    vector<vector<int>>a(n,vector<int>(n,OO));
+    for (int i = 0; i < m; ++i) {
+        ll u,v;cin>>u>>v;
+        ll w;
+        cin>>w;
+        u--,v--;
+        a[u][v]=min(a[u][v],w);
+    }
+    a= expo(a,k);
+    if(a[0][n-1]>=OO)cout<<"-1\n";
+    else cout<<a[0][n-1];
+}
+signed main() {
+    //freopen("input.txt","rt",stdin);
+    //freopen("output.txt","wt",stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t=1;
+//    cin>>t;
+    ll index=1;
+    while (t--){
+        func(index++);
+    }
+    return 0;
+}
